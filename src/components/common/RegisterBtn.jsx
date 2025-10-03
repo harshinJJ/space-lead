37261f0879d1b248046ca8e94afc7fd01087404b
Muhammed Ajmal <ajmal@veuz>
2025-10-03 22:25:30 +0530

    register btn fix

--- src/components/common/RegisterBtn.jsx
+++ src/components/common/RegisterBtn.jsx
@@ -12,7 +12,7 @@ const RegisterBtn = ({
     <PrimaryLink
       href={href}
       {...props}
-      className={` leading-[1]  btn-gradient text-3xl py-3 px-10.25 rounded-br-[0] rounded-bl-[0]  rounded-tr-[1rem] rounded-tl-[1rem]  ${className}`}
+      className={`relative leading-[1]  btn-gradient text-3xl py-3 px-10.25 rounded-br-[0] rounded-bl-[0]  rounded-tr-[1rem] rounded-tl-[1rem] pointer-events-auto ${className}`}
     >
       {children}
     </PrimaryLink></div>
