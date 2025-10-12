6fd6174512e59f684e7ccdfde4a127a95e7b7f1d
mshibinc <m.shibin.c@gmail.com>
2025-10-12 22:30:12 +0530

    gap-2 speaker

--- src/app/(common)/speakers/components/MemberPreview.jsx
+++ src/app/(common)/speakers/components/MemberPreview.jsx
@@ -31,7 +31,7 @@ const MemberPreview = ({ speakerList = [], activeSpeaker }) => {
           <div className="container-fluid w-full mx-auto flex flex-col items-center lg:items-start lg:flex-row xl:gap-8.75 gap-5 xl:px-15.75 px-5">
             <div className="xl:w-1/4 lg:w-2/5 sm:max-w-3/4 bg- md:1/2 w-full">
               {/* <SpeakerCard speaker={activeSpeaker} showBtn={true} /> */}
-              <div className="flex flex-col items-center gap-7 px-4 py-5 rounded-2xl bg-primary">
+              <div className="flex flex-col items-center gap-2 px-4 py-5 rounded-2xl bg-primary">
                 <div className=" aspect-[298/272] w-full rounded-2xl overflow-hidden relative ">
                   <Image
                     fill
@@ -54,31 +54,35 @@ const MemberPreview = ({ speakerList = [], activeSpeaker }) => {
                   {activeSpeaker?.name ||
                     `${activeSpeaker.firstname} ${activeSpeaker.lastname}`}
                 </h3>
-                <p className="text-white text-center text-sm">{activeSpeaker?.designation}</p>
+                <p className="text-white text-center text-sm">
+                  {activeSpeaker?.designation}
+                </p>
               </div>
             </div>
-            {activeSpeaker&&<div className="flex-3/4 w-full">
-              <div className="flex bg bg-gradient-to-r from-white to-transparent w-full rounded-l-lg p-1 items-center  border border-[#E4E4E7]">
-                {["About", "Agenda"].map((tab, i) => (
-                  <PrimaryButton
-                    key={i}
-                    onClick={() => setActiveTab(i)}
-                    className={`px-5 sm:px-8 !py-1 rounded-lg flex-col relative ${
-                      activeTab === i
-                        ? ""
-                        : " !bg-[url('')] from-transparent !text-secondary to-transparent"
-                    }`}
-                  >
-                    {tab}
-                  </PrimaryButton>
-                ))}
+            {activeSpeaker && (
+              <div className="flex-3/4 w-full">
+                <div className="flex bg bg-gradient-to-r from-white to-transparent w-full rounded-l-lg p-1 items-center  border border-[#E4E4E7]">
+                  {["About", "Agenda"].map((tab, i) => (
+                    <PrimaryButton
+                      key={i}
+                      onClick={() => setActiveTab(i)}
+                      className={`px-5 sm:px-8 !py-1 rounded-lg flex-col relative ${
+                        activeTab === i
+                          ? ""
+                          : " !bg-[url('')] from-transparent !text-secondary to-transparent"
+                      }`}
+                    >
+                      {tab}
+                    </PrimaryButton>
+                  ))}
+                </div>
+                {activeTab === 0 && <SpeakerAbout speaker={activeSpeaker} />}
+                {activeTab === 1 && (
+                  <SpeakerAgenda agenda={activeSpeaker?.agenda_sessions} />
+                )}
+                {/* <div className="hidden lg:block border-b lg:w-6/10 lg:mt-6 border-[#D7D7D7]" /> */}
               </div>
-              {activeTab === 0 && <SpeakerAbout speaker={activeSpeaker} />}
-              {activeTab === 1 && (
-                <SpeakerAgenda agenda={activeSpeaker?.agenda_sessions} />
-              )}
-              {/* <div className="hidden lg:block border-b lg:w-6/10 lg:mt-6 border-[#D7D7D7]" /> */}
-            </div>}
+            )}
           </div>
           <div className="hidden lg:block border-b lg:w-1/3 mx-auto lg:mt-6 border-[#D7D7D7]" />
         </section>
